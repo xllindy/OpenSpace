3fdd3733b52e6dda1265a0c3f44f34a11015c770
L.M.F. Rutten <539663@student.fontys.nl>
2024-01-11 16:59:02 +0100

    Document loaded

--- Login.js
+++ Login.js
@@ -1,39 +1,46 @@
-function login() {
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById("registrationForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    submitRegistrationForm();
+  });
+    
+  function login() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
-  const xhttp = new XMLHttpRequest();
-  xhttp.open('POST', 'http://192.168.157.123:3000/customers/login');
-  xhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('POST', 'http://192.168.157.123:3000/customers/login');
+    xhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
 
-  xhttp.send(JSON.stringify({
-    email: email,
-    password: password
-  }));
+    xhttp.send(JSON.stringify({
+      email: email,
+      password: password
+    }));
 
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4) {
-      const response = JSON.parse(this.responseText);
-      console.log(response);
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        const response = JSON.parse(this.responseText);
+        console.log(response);
 
-      if (response.status === 'ok') {
-        Swal.fire({
-          text: response.message,
-          icon: 'success',
-          confirmButtonText: 'OK'
-        }).then((result) => {
-          if (result.isConfirmed) {
-            window.location.href = './index.html';
-          }
-        });
-      } else {
-        Swal.fire({
-          text: response.message,
-          icon: 'error',
-          confirmButtonText: 'OK'
-        });
+        if (response.status === 'ok') {
+          Swal.fire({
+            text: response.message,
+            icon: 'success',
+            confirmButtonText: 'OK'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              window.location.href = './index.html';
+            }
+          });
+        } else {
+          Swal.fire({
+            text: response.message,
+            icon: 'error',
+            confirmButtonText: 'OK'
+          });
+        }
       }
-    }
-  };
-  return false;
-}
+    };
+    return false;
+  }
+});
