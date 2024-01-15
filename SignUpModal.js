102a16c9ea258ab66b58a09c90bfd6e2cfe722f0
L.M.F. Rutten <539663@student.fontys.nl>
2024-01-15 11:32:43 +0100

    Fixes SignUp
    API Still doesnt respond

--- SignUpModal.js
+++ SignUpModal.js
@@ -1,3 +1,56 @@
+async function registerAccount(Name, Email_Adress, Phone_Number, Password,IBAN, License_Plate, isDisabled){
+    try{ let response = await fetch("http://192.168.157.123:3000/customers", {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            Name: Name,
+            Email_Adress: Email_Adress,
+            Phone_Number: Phone_Number,
+            Password: Password,
+            IBAN: IBAN,
+            License_Plate: License_Plate,
+            Disabled: isDisabled,
+        })
+    })
+    let json = await response.json();
+    console.log(json);
+    switch(json.message){
+        case "Name is required":
+            window.alert("Please enter a valid username");
+            break;
+        case "Email is required":
+            window.alert("Please enter a valid email adress");
+            break;
+        case "Password is required":
+            window.alert("Please enter a valid password");
+            break;
+        case "IBAN is invaled":
+            window.alert("Please enter a valid IBAN");
+            break;
+        case "License plate is invalid":
+            window.alert("Please enter a valid license plate");
+            break;
+        case "Phonen number is invalid":
+            window.alert("Please enter a valid phone number");
+        case "Customer already exists":
+            window.alert("A account with this username and email already exists");
+            break;
+        case "Customer created successfully":
+            window.alert("Signup Succesful!");
+            break;
+        default:
+            window.alert("Something went wrong, please try again later")
+            break;
+    }
+    } catch (error) {
+        console.log(error);
+        window.alert("Error", "An error has occurred" + error);
+    }
+}
+
 function submitRegistrationForm() {
     var Name = document.getElementById("name").value;
     var Email_Adress = document.getElementById("email").value;
@@ -6,43 +59,11 @@ function submitRegistrationForm() {
     var IBAN = document.getElementById("iban").value;
     var License_Plate = document.getElementById("license_plate").value;
     var legalTermsCheckbox = document.getElementById("legal_terms");
+    var isDisabled = document.getElementById("isDisabled");
 
-    if (Name === "" || Email_Adress === "" || Password === "" || Phone_Number === "" || IBAN === "" || License_Plate === "" || !legalTermsCheckbox.checked) {
-        alert("Please fill in all the required fields and agree to the legal terms.");
-        return;
-    }
-
-    var data = {
-        Name: Name,
-        Email_Adress: Email_Adress,
-        Phone_Number: Phone_Number,
-        Password: Password,
-        IBAN: IBAN,
-        License_Plate: License_Plate,
-    };
-
-    fetch("http://192.168.157.123:3000/customers", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        else {
-            alert("Signup successful!");
-            window.location.href = "";
-        }
-    })
-    
-    .catch(error => {
-        console.error("Error during signup:", error);
-        alert("An error occurred during signup. Please try again later.");
-    });
+    registerAccount(Name,Email_Adress, Phone_Number, Password, IBAN, License_Plate,legalTermsCheckbox, isDisabled);
 }
+
 document.getElementById("registrationForm").addEventListener("submit", function (event) {
     event.preventDefault();
     submitRegistrationForm();
